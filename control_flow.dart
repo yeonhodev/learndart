f81e29a956ae4364627c4e9691a878bc34af9189
yeonhodev <yeonho.dev@gmail.com>
2021-03-11 23:48:36 -0500

    Create control_flow.dart

---
+++ control_flow.dart
@@ -0,0 +1,24 @@
+bool isEven(int x) {
+  // An if-else statement.
+  if (x % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+List<int> getEvenNumbers(Iterable<int> numbers) {
+  var evenNumbers = <int>[];
+  
+  // A for-in loop.
+  for (var i in numbers) {
+    // A single-line if statement.
+    if (isEven(i)) evenNumbers.add(i);
+  }
+  return evenNumbers;
+}
+
+main() {
+  List<int> numbers = List.generate(10, (i) => i);
+  print(getEvenNumbers(numbers));
+}
